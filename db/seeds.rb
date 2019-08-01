c1335f43aa511b3a26fb2b34fd22c4f9dc35cc4a
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 23:37:34 +0000

    seed info for games

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Game.create(title: "Super Mario Odyssey", publisher: "Nintendo", description: "Help Mario save the Princess from the evil King Bowswer!")
+Game.create(title: "Kirby Star Allies", publisher: "Nintendo", description: "Kirby needs to save the universe again from another cosmic horror threat.")
+Game.create(title: "Legend of Zelda: Breath of the Wild", publisher: "Nintendo", description: "Link isn't in Kansas anymore.")
+Game.create(title: "Mario Party", publisher: "Nintendo", description: "Have fun destroying friendships in this 4 player party game.")
+Game.create(title: "New Super Mario Bros", publisher: "Nintendo", description: "Classic Mario fun!")
+
+Game.create(title: "Resident Evil 6", publisher: "Capcom", description: "Action survival returns with more monsters to suplex.")
+Game.create(title: "Resident Evil 7", publisher: "Capcom", description: "First person survival with one crazy family.")
+
+Game.create(title: "Halo", publisher: "Microsoft", description: "Masterchief must find a way back home.")
+Game.create(title: "Minecraft", publisher: "Microsoft", description: "It's a blocky world out there, becareful!")
