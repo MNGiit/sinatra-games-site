37fe0c79306457538be4cba7ab27a2f4b705908f
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 18:44:11 +0000

    user attributes name, email, password

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
+      t.string :password # don't forget to make this password_digest
     end
   end
 end
