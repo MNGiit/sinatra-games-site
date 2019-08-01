1f1bf0e20ea340b91a39b9330dd05fc93c934f78
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 20:37:17 +0000

    add post /signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
     erb :'users/signup'
   end
   
+  post '/signup' do
+    # info for name, email, or password is missing? redirect to /signup
+    if params[:name].nil? || params[:email].nil? || params[:password].nil?
+      redirect to '/signup'
+    else
+      "Looks like params is legit."
+    end
+    
+  end
+  
   get '/login' do
     "Place holder for login. Don't forget to do a Post"
   end
