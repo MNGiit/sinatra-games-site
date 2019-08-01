46e52a05cd99b8dfaae3e439dea30359431abe06
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 21:11:55 +0000

    user has_secure_password

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password # don't forget to make this password_digest
+      t.string :password_digest # don't forget to make this password_digest
     end
   end
 end
