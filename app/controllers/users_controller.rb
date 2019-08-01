2718059225a4d8a2177f5cadb580614ae5899c86
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 18:56:12 +0000

    users, signup, login, logout paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
     "So far, so good."
   end
   
+  get '/signup' do
+    "Place holder for signup. Don't forget to do a Post."
+  end
+  
+  get '/login' do
+    "Place holder for login. Don't forget to do a Post"
+  end
+  
+  get '/logout' do
+    "Placeholder for logout."
+  end
+  
+  # /users/:id
 end
