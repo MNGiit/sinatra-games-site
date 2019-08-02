1fbc07566b3f19bcf09156b694467edb907807e9
Mohammed Noori <kmanoorib@gmail.com>
2019-08-02 23:26:32 +0000

    add games selection to form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,8 @@ class ReviewsController < ApplicationController
   
   get '/reviews/new' do
     if session[:user_id]
+      @games = Game.all
+      binding.pry
       erb :'reviews/new'
     else
       redirect to '/login'
@@ -9,6 +11,7 @@ class ReviewsController < ApplicationController
   end
   
   post '/reviews/new' do
+    binding.pry
     "It did a post review new"
   end
   
