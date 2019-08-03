c539693bdcb8996a26dff07182e5da4cf6c7ac26
Mohammed Noori <kmanoorib@gmail.com>
2019-08-03 00:07:35 +0000

    add a way to find a review by user id and game id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,11 @@ class ReviewsController < ApplicationController
   
   post '/reviews/new' do
     "It did a post review new"
+ 
+    # https://stackoverflow.com/questions/3936621/rails-find-by-with-2-fields
+    # find_by(user_id: params[:user_id], project_id: params[:project_id])
+    review = Review.find_by(:user_id => session[:user_id], :game_id => params[:game_id])
+    binding.pry
     @review = Review.new(:score => params[:score], :content => params[:content], :user => User.find_by_id(session[:user_id]), :game => Game.find(params[:game_id]))
     "#{@review.score} - #{@review.content} - #{@review.user.name} - #{@review.game.title}"
   end
