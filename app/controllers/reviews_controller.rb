a42a76a7128cd059e8d5adb504c46135274c2a56
Mohammed Noori <kmanoorib@gmail.com>
2019-08-03 00:14:53 +0000

    new review only if review doesn't exist

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,14 +10,16 @@ class ReviewsController < ApplicationController
   end
   
   post '/reviews/new' do
-    "It did a post review new"
- 
     # https://stackoverflow.com/questions/3936621/rails-find-by-with-2-fields
     # find_by(user_id: params[:user_id], project_id: params[:project_id])
     review = Review.find_by(:user_id => session[:user_id], :game_id => params[:game_id])
     binding.pry
-    @review = Review.new(:score => params[:score], :content => params[:content], :user => User.find_by_id(session[:user_id]), :game => Game.find(params[:game_id]))
+    if review
+      "Review already made."
+    else
+      @review = Review.new(:score => params[:score], :content => params[:content], :user => User.find_by_id(session[:user_id]), :game => Game.find(params[:game_id]))
     "#{@review.score} - #{@review.content} - #{@review.user.name} - #{@review.game.title}"
+    end
   end
   
 end
